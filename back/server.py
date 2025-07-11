f46753e80e6029c3b32fc96bcefdc3a83dc6695f
Seth Samuel <Seths@missionreadyhq.com>
2025-07-11 19:47:06 +1200

    connection to backend openai works and removed .env

--- back/server.py
+++ back/server.py
@@ -1,28 +1,52 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-load_dotenv() 
+from typing import Final
+load_dotenv()
 
 client = OpenAI()
+AI_MODEL: Final[str] = "gpt-4.1"
 
-response = client.chat.completions.create(
-  model="gpt-4.1",
-  messages=[
+response = client.responses.create(
+  model=AI_MODEL,
+  input=[
     {
       "role": "system",
       "content": [
         {
-          "type": "text",
-          "text": "your frendly and helpfull "
+          "type": "input_text",
+          "text": "your frendly and helpfull"
+        }
+      ]
+    },
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "input_text",
+          "text": "hello"
         }
       ]
     }
   ],
-  response_format={
-    "type": "text"
+  text={
+    "format": {
+      "type": "text"
+    }
   },
+  reasoning={},
+  tools=[],
   temperature=1,
-  max_completion_tokens=2048,
+  max_output_tokens=2048,
   top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
+  store=True
 )
+
+while True:
+    print(f" AI: {response.output_text}")
+    user_input: str = input()
+    response = client.responses.create(
+        model=AI_MODEL,
+        previous_response_id=response.id,
+        input=[{"role": "user", "content": user_input}],
+    )
+    
