6409edef700fd598cad5428e3b6b77591f561526
Seth Samuel <Seths@missionreadyhq.com>
2025-07-18 13:00:37 +1200

    first attempt at voice chat

--- front/src/app/voice-chat/page.tsx
+++ front/src/app/voice-chat/page.tsx
@@ -1,22 +1,67 @@
 "use client";
+import { connect } from "http2";
 import React, { useEffect, useRef } from "react";
-// import { OwnerEnum } from "@/types/typedef";
+const BACKEND_SERVER_BASE_URL = process.env.BACKEND_SERVER_BASE_URL;
 
 export default function page() {
-  const wsRef = useRef<WebSocket | null>(null);
-  useEffect(()=>{
-    const websocket = new WebSocket("ws://localhost:8000/chat-ws-voice");
-    wsRef.current = websocket;
+  // crate a peer connection
+  useEffect(() => {
+    //get key from backend
+    async function initVoice() {
+      const tokenRespose = await fetch(`${BACKEND_SERVER_BASE_URL}/key-for-webrtc`);
+      const data = await tokenRespose.json();
+      const AI_EPHEMERAL_KEY = data.client_secret.value;
 
-    websocket.onopen = () =>{
-      console.log("Websocket connected")
+      // create a peer connection
+      const peerConnection = new RTCPeerConnection();
+
+      // Set up to play remote audio from model
+      const audioEl = document.createElement("audio");
+      audioEl.autoplay = true;
+      peerConnection.ontrack = (e) => {
+        audioEl.srcObject = e.streams[0];
+      };
+
+      //add local as input
+      const micDevice = await navigator.mediaDevices.getUserMedia({ audio: true });
+      peerConnection.addTrack(micDevice.getTracks()[0]);
+
+      //set data channel for sending and receiving events
+      const dataChannel = peerConnection.createDataChannel("oai-events");
+      dataChannel.addEventListener("message", (e) => {
+        console.log(e);
+      });
+
+      //start session
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+
+      const AI_BASE_URL = "https://api.openai.com/v1/realtime";
+      const MODEL = process.env.OPENAI_VOICE_MODEL;
+      const sdpResponse = await fetch(`${AI_BASE_URL}?model=${MODEL}`, {
+        method: "POST",
+        body: offer.sdp,
+        headers: {
+          Authorization: `Bearer ${AI_EPHEMERAL_KEY}`,
+          "Content-Type": "application/sdp",
+        },
+      });
+
+      const answerFromSdpRequest: RTCSessionDescriptionInit = {
+        type: "answer",
+        sdp: await sdpResponse.text(),
+      };
+
+      await peerConnection.setRemoteDescription(answerFromSdpRequest);
     }
 
-    websocket.onmessage = (event)=>{
+    initVoice();
+    return () => {};
+  }, []);
 
-
-    }
-  },[])
-
-  return <div></div>;
+  return (
+    <div>
+      <p>voice</p>
+    </div>
+  );
 }
